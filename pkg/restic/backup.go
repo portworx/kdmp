3574800753b389f26596e4ec35390869dcd96a4c
Aditya Dani <aditya@portworx.com>
2020-02-06 13:20:05 -0800

    Add wrappers for restic backup command.
    - Implement a command factory for all restic cli commands.
    - Add two new interfaces Command and Executor.
      - Command is a part of the restic command factory
      - Executor is an interface to run long running commands and query its status.
    - Add implementations for backup restic cli.

---
+++ pkg/restic/backup.go
@@ -0,0 +1,212 @@
+package restic
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"sync"
+)
+
+// GetBackupCommand returns a wrapper over the restic backup
+// command
+func GetBackupCommand(
+	repoName string,
+	secretFilePath string,
+	srcPath string,
+) (*Command, error) {
+	if srcPath == "" {
+		return nil, fmt.Errorf("source path cannot be empty")
+	}
+	if repoName == "" {
+		return nil, fmt.Errorf("repository name cannot be empty")
+	}
+	if secretFilePath == "" {
+		return nil, fmt.Errorf("secret file path cannot be empty")
+	}
+
+	return &Command{
+		Name:           "backup",
+		RepositoryName: repoName,
+		SecretFilePath: secretFilePath,
+		Flags:          defaultFlags(),
+		Args:           []string{srcPath},
+	}, nil
+}
+
+// BackupProgressResponse is the json representation of the in-progress
+// output of restic backup
+type BackupProgressResponse struct {
+	MessageType      string   `json:"message_type"` // "status"
+	SecondsElapsed   uint64   `json:"seconds_elapsed,omitempty"`
+	SecondsRemaining uint64   `json:"seconds_remaining,omitempty"`
+	PercentDone      float64  `json:"percent_done"`
+	TotalFiles       uint64   `json:"total_files,omitempty"`
+	FilesDone        uint64   `json:"files_done,omitempty"`
+	TotalBytes       uint64   `json:"total_bytes,omitempty"`
+	BytesDone        uint64   `json:"bytes_done,omitempty"`
+	ErrorCount       uint     `json:"error_count,omitempty"`
+	CurrentFiles     []string `json:"current_files,omitempty"`
+}
+
+// BackupSummaryResponse is the json representation of the summary output
+// of restic backup
+type BackupSummaryResponse struct {
+	MessageType         string  `json:"message_type"` // "summary"
+	FilesNew            uint    `json:"files_new"`
+	FilesChanged        uint    `json:"files_changed"`
+	FilesUnmodified     uint    `json:"files_unmodified"`
+	DirsNew             uint    `json:"dirs_new"`
+	DirsChanged         uint    `json:"dirs_changed"`
+	DirsUnmodified      uint    `json:"dirs_unmodified"`
+	DataBlobs           int     `json:"data_blobs"`
+	TreeBlobs           int     `json:"tree_blobs"`
+	DataAdded           uint64  `json:"data_added"`
+	TotalFilesProcessed uint    `json:"total_files_processed"`
+	TotalBytesProcessed uint64  `json:"total_bytes_processed"`
+	TotalDuration       float64 `json:"total_duration"` // in seconds
+	SnapshotID          string  `json:"snapshot_id"`
+}
+
+type backupExecutor struct {
+	cmd             *Command
+	responseLock    sync.Mutex
+	summaryResponse *BackupSummaryResponse
+	execCmd         *exec.Cmd
+	outBuf          *bytes.Buffer
+	errBuf          *bytes.Buffer
+	lastError       error
+	isRunning       bool
+}
+
+// NewBackupExecutor returns an instance of Executor that can be used for
+// running a restic backup command
+func NewBackupExecutor(cmd *Command) Executor {
+	return &backupExecutor{
+		cmd:    cmd,
+		outBuf: new(bytes.Buffer),
+		errBuf: new(bytes.Buffer),
+	}
+}
+
+func (b *backupExecutor) Run() error {
+	b.responseLock.Lock()
+	defer b.responseLock.Unlock()
+
+	if b.isRunning {
+		return fmt.Errorf("another backup operation is already running")
+	}
+
+	b.execCmd = b.cmd.Cmd()
+	b.execCmd.Stdout = b.outBuf
+	b.execCmd.Stderr = b.errBuf
+
+	if err := b.execCmd.Start(); err != nil {
+		b.lastError = err
+		return err
+	}
+	b.isRunning = true
+	go func() {
+		b.execCmd.Wait()
+		// backup has completed
+		b.responseLock.Lock()
+		defer b.responseLock.Unlock()
+
+		summaryResponse, err := getSummary(b.outBuf.Bytes(), b.errBuf.Bytes())
+		if err != nil {
+			b.lastError = err
+			return
+		}
+		b.summaryResponse = summaryResponse
+	}()
+	return nil
+}
+
+func (b *backupExecutor) Status() (*Status, error) {
+	b.responseLock.Lock()
+	defer b.responseLock.Unlock()
+
+	if b.lastError != nil {
+		return &Status{
+			LastKnownError: b.lastError,
+			Done:           true,
+		}, nil
+	}
+
+	if b.summaryResponse != nil {
+		return &Status{
+			ProgressPercentage:  100,
+			TotalBytesProcessed: b.summaryResponse.TotalBytesProcessed,
+			TotalBytes:          b.summaryResponse.TotalBytesProcessed,
+			SnapshotID:          b.summaryResponse.SnapshotID,
+			Done:                true,
+			LastKnownError:      nil,
+		}, nil
+	} // else backup is still in progress
+	progressResponse, err := getProgress(b.outBuf.Bytes(), b.errBuf.Bytes())
+	if err != nil {
+		return &Status{
+			Done:           false,
+			LastKnownError: err,
+		}, nil
+	}
+
+	return &Status{
+		ProgressPercentage:  progressResponse.PercentDone * 100,
+		TotalBytes:          progressResponse.TotalBytes,
+		TotalBytesProcessed: progressResponse.BytesDone,
+		Done:                false,
+		LastKnownError:      nil,
+	}, nil
+}
+
+func getSummary(outBytes []byte, errBytes []byte) (*BackupSummaryResponse, error) {
+	outLines := bytes.Split(outBytes, []byte("\n"))
+
+	if len(outLines) <= 2 {
+		return nil, &Error{
+			Reason:    fmt.Sprintf("backup summary not available"),
+			CmdOutput: string(outBytes),
+			CmdErr:    string(errBytes),
+		}
+	}
+
+	for i := len(outLines) - 1; i >= 0; i-- {
+		summaryResponse := &BackupSummaryResponse{}
+		if len(outLines[i]) < 1 {
+			// ignore \n
+			continue
+		}
+		if err := json.Unmarshal(outLines[i], summaryResponse); err != nil {
+			return nil, &Error{
+				Reason:    fmt.Sprintf("failed to parse backup summary: %v", err),
+				CmdOutput: string(outLines[i]),
+				CmdErr:    string(errBytes),
+			}
+		}
+		if summaryResponse.MessageType != "summary" {
+			continue
+		}
+		return summaryResponse, nil
+	}
+	return nil, &Error{
+		Reason: fmt.Sprintf("could not find backup summary"),
+	}
+}
+
+func getProgress(outBytes []byte, errBytes []byte) (*BackupProgressResponse, error) {
+	outLines := bytes.Split(outBytes, []byte("\n"))
+	if len(outLines) <= 2 {
+		return nil, fmt.Errorf("backup progress not available yet")
+	}
+	outLine := outLines[len(outLines)-2] // last line is \n
+	progressResponse := &BackupProgressResponse{}
+	if err := json.Unmarshal(outLine, progressResponse); err != nil || len(progressResponse.MessageType) == 0 {
+		return nil, &Error{
+			Reason:    fmt.Sprintf("failed to parse progress of backup: %v", err),
+			CmdOutput: string(outLine),
+			CmdErr:    string(errBytes),
+		}
+	}
+	return progressResponse, nil
+}
---
+++ pkg/restic/backup_test.go
@@ -0,0 +1,198 @@
+package restic
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testRepoName       = "/tmp/kdmp-restic-tests"
+	testSource         = "/tmp/source"
+	testSecretFilePath = "/tmp/secret"
+)
+
+func TestGetBackupCommand(t *testing.T) {
+	cmd, err := GetBackupCommand("fo", "bar", "")
+	require.Error(t, err, "expected an error from GetBackupCommand")
+	require.Nil(t, cmd, "expected a nil Command")
+
+	cmd, err = GetBackupCommand("foo", "", "src")
+	require.Error(t, err, "expected an error from GetBackupCommand")
+	require.Nil(t, cmd, "expected a nil Command")
+
+	cmd, err = GetBackupCommand("", "bar", "src")
+	require.Error(t, err, "expected an error from GetBackupCommand")
+	require.Nil(t, cmd, "expected a nil Command")
+
+	cmd, err = GetBackupCommand("foo", "bar", "src")
+	require.NoError(t, err, "unexpected error on GetBackupCommand")
+
+	actualCmd := cmd.Cmd()
+	expectedCmd := exec.Command("restic", "backup", "--repo", "foo", "--password-file", "bar", "--host", "kdmp", "--json", "src")
+	require.Equal(t, actualCmd, expectedCmd, "unexpected command parsed")
+
+	cmd.AddArg("arg1")
+
+	actualCmd = cmd.Cmd()
+	expectedCmd = exec.Command("restic", "backup", "--repo", "foo", "--password-file", "bar", "--host", "kdmp", "--json", "src", "arg1")
+	require.Equal(t, actualCmd, expectedCmd, "unexpected command parsed")
+
+	cmd.AddFlag("--flag").AddFlag("flag1")
+
+	actualCmd = cmd.Cmd()
+	expectedCmd = exec.Command("restic", "backup", "--repo", "foo", "--password-file", "bar", "--host", "kdmp", "--json", "--flag", "flag1", "src", "arg1")
+	require.Equal(t, actualCmd, expectedCmd, "unexpected command parsed")
+
+}
+
+func TestGetProgress(t *testing.T) {
+	testData := `
+{"message_type":"status","percent_done":0,"total_files":1,"total_bytes":10485760}
+{"message_type":"status","percent_done":0.5,"total_files":1,"total_bytes":10485760,"bytes_done":5242880,"current_files":["/tmp/source/10M"]}
+{"message_type":"status","percent_done":0.75,"total_files":1,"total_bytes":10485760,"bytes_done":7864320,"current_files":["/tmp/source/10M"]}
+{"message_type":"status","percent_done":0.95,"total_files":1,"total_bytes":10485760,"bytes_done":9961472,"current_files":["/tmp/source/10M"]}
+`
+	testBytes := []byte(testData)
+	backupProgress, err := getProgress(testBytes, nil)
+	require.NoError(t, err, "unexpected error on get progress")
+	require.Equal(t, float64(0.95), backupProgress.PercentDone, "unexpected percentDone")
+	require.Equal(t, uint64(9961472), backupProgress.BytesDone, "unexpected bytesDone")
+	require.Equal(t, uint64(10485760), backupProgress.TotalBytes, "unexpected totalBytes")
+}
+
+func TestGetProgressNotAvailable(t *testing.T) {
+	testData := `
+`
+	testBytes := []byte(testData)
+	backupProgress, err := getProgress(testBytes, nil)
+	require.EqualError(t, err, "backup progress not available yet", "unexpected error")
+	require.Nil(t, backupProgress, "expected nil backup progress")
+}
+
+func TestGetProgressUnmarshalFailure(t *testing.T) {
+	testData := `
+{"foo":"bar"}
+`
+	testBytes := []byte(testData)
+	errBytes := testBytes
+
+	backupProgress, err := getProgress(testBytes, errBytes)
+	require.Error(t, err, "expected an error on getProgress")
+	require.Nil(t, backupProgress, "expected nil backup progress")
+	bErr, ok := err.(*Error)
+	require.True(t, ok, "expected error of Error kind")
+	require.Contains(t, bErr.Reason, "failed to parse progress of backup")
+	require.Equal(t, string(errBytes), bErr.CmdErr, "unexpected cmdErr")
+	require.Equal(t, string(testBytes), bErr.CmdErr, "unexpected cmdOutput")
+}
+
+func TestGetSummary(t *testing.T) {
+	testData := `
+{"message_type":"status","percent_done":0.3,"total_files":1,"total_bytes":10485760,"bytes_done":3145728,"current_files":["/tmp/source/10M"]}
+{"message_type":"status","percent_done":0.55,"total_files":1,"total_bytes":10485760,"bytes_done":5767168,"current_files":["/tmp/source/10M"]}
+{"message_type":"status","percent_done":0.85,"total_files":1,"total_bytes":10485760,"bytes_done":8912896,"current_files":["/tmp/source/10M"]}
+{"message_type":"summary","files_new":1,"files_changed":0,"files_unmodified":0,"dirs_new":1,"dirs_changed":0,"dirs_unmodified":0,"data_blobs":1,"tree_blobs":2,"data_added":525023,"total_files_processed":1,"total_bytes_processed":10485760,"total_duration":0.541563334,"snapshot_id":"9310620e"}
+`
+
+	testBytes := []byte(testData)
+	backupSummary, err := getSummary(testBytes, nil)
+	require.NoError(t, err, "unexpected error on getSummary")
+	require.Equal(t, uint64(10485760), backupSummary.TotalBytesProcessed)
+	require.Equal(t, "summary", backupSummary.MessageType, "unexpected message")
+	require.Equal(t, "9310620e", backupSummary.SnapshotID, "unexpected snapshot_id")
+}
+
+func TestGetSummaryNotAvailable(t *testing.T) {
+	testData := `
+`
+	testBytes := []byte(testData)
+	backupSummary, err := getSummary(testBytes, nil)
+	require.Nil(t, backupSummary, "expected nil backup summary")
+	bErr, ok := err.(*Error)
+	require.True(t, ok, "unexpected error type")
+	require.Equal(t, bErr.Reason, "backup summary not available")
+}
+
+func TestGetSummaryUnmarshalFailure(t *testing.T) {
+	testData := `
+adssdgsg
+sgssdfsafd
+`
+	testBytes := []byte(testData)
+	backupSummary, err := getSummary(testBytes, nil)
+	require.Nil(t, backupSummary, "expected nil backup summary")
+	bErr, ok := err.(*Error)
+	require.True(t, ok, "unexpected error type")
+	require.Contains(t, bErr.Reason, "failed to parse backup summary")
+}
+
+func TestGetSummaryNotFound(t *testing.T) {
+	testData := `
+{"message_type":"status","percent_done":0.3,"total_files":1,"total_bytes":10485760,"bytes_done":3145728,"current_files":["/tmp/source/10M"]}
+{"message_type":"status","percent_done":0.55,"total_files":1,"total_bytes":10485760,"bytes_done":5767168,"current_files":["/tmp/source/10M"]}
+{"message_type":"status","percent_done":0.85,"total_files":1,"total_bytes":10485760,"bytes_done":8912896,"current_files":["/tmp/source/10M"]}
+`
+	testBytes := []byte(testData)
+	backupSummary, err := getSummary(testBytes, nil)
+	require.Nil(t, backupSummary, "expected nil backup summary")
+	bErr, ok := err.(*Error)
+	require.True(t, ok, "unexpected error type")
+	require.Equal(t, bErr.Reason, "could not find backup summary")
+}
+
+func TestBackupExecutor(t *testing.T) {
+	testSetup(t)
+	defer testTeardown(t)
+
+	bc, err := GetBackupCommand(testRepoName, testSecretFilePath, testSource)
+	require.NoError(t, err, "unexpected error on GetBackupCommand")
+
+	be := NewBackupExecutor(bc)
+	require.NoError(t, be.Run(), "unexpected error on backup executor run")
+
+	foundSummary := false
+	// TODO: remove the dependency of this UT on time
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Second)
+		status, err := be.Status()
+		require.NoError(t, err, "unexpected error on backup executor status")
+		require.NotNil(t, status, "unexpected nil status")
+		if status.Done {
+			require.Equal(t, float64(100), status.ProgressPercentage, "unexpected progress percentage")
+			require.Equal(t, status.TotalBytes, status.TotalBytesProcessed, "unexpected total bytes")
+			foundSummary = true
+			break
+		} else {
+
+			require.True(t, status.TotalBytesProcessed <= status.TotalBytes, "unexpected total bytes")
+		}
+	}
+	require.True(t, foundSummary, "expected a summary")
+}
+
+func testTeardown(t *testing.T) {
+	os.RemoveAll(testSource)
+	os.RemoveAll(testRepoName)
+	os.Remove(testSecretFilePath)
+}
+
+func testSetup(t *testing.T) {
+	testTeardown(t)
+
+	err := ioutil.WriteFile(testSecretFilePath, []byte("test123"), 0644)
+	require.NoError(t, err, "unexpected error on creating secret file")
+
+	cmd := exec.Command("restic", "-r", testRepoName, "-p", testSecretFilePath, "init")
+	out, err := cmd.CombinedOutput()
+	require.NoError(t, err, "unexpected error on repo init: ", string(out))
+
+	os.MkdirAll(testSource, os.ModeDir)
+	ddCmd := exec.Command("dd", "if=/dev/zero", "of=/tmp/source/10M", "bs=1M", "count=10")
+	out, err = ddCmd.CombinedOutput()
+	require.NoError(t, err, "unexpected error on source generation: ", string(out))
+}
