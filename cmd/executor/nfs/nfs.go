1334830d2852d6a1b4c9c2a464990e15240d7432
Prashanth Kumar <prkumar@purestorage.com>
2022-08-23 10:33:58 +0000

    vendor update for stork storage drivers

--- cmd/executor/nfs/nfs.go
+++ cmd/executor/nfs/nfs.go
@@ -3,6 +3,11 @@ package main
 import (
 	"os"
 
+	_ "github.com/libopenstorage/stork/drivers/volume/aws"
+	_ "github.com/libopenstorage/stork/drivers/volume/azure"
+	_ "github.com/libopenstorage/stork/drivers/volume/csi"
+	_ "github.com/libopenstorage/stork/drivers/volume/gcp"
+	_ "github.com/libopenstorage/stork/drivers/volume/portworx"
 	nfs_executor "github.com/portworx/kdmp/pkg/executor/nfs"
 )
 
