48f2a2dd3c162d15933352c9c5ce8d90e7cf8c3d
sivakumar subraani <sivakumar subramani>
2023-03-24 19:24:30 +0000

    pb-3733: fixed the issue in pass region parameter in connect cmd for non aws cases.
    Signed-off-by: Sivakumar Subramani <ssubramani@purestorage.com>

--- pkg/executor/kopia/kopiabackup.go
+++ pkg/executor/kopia/kopiabackup.go
@@ -360,8 +360,8 @@ func runKopiaRepositoryConnect(repository *executor.Repository) error {
 			repository.Path,
 			repository.Name,
 			repository.Password,
+			kopiaProvderType[repository.Type],
 			"",
-			kopiaProviderType[repository.Type],
 			false,
 		)
 	}
