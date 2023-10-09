4a1cf78b486d70e4d9338b2a2bf7abcd1502f9e3
sivakumar subraani <sivakumar subramani>
2023-10-09 17:35:08 +0000

    pb-4579: Passing --sseType to kopia only when S3Config.SseType is passed.

--- pkg/executor/kopia/kopiabackup.go
+++ pkg/executor/kopia/kopiabackup.go
@@ -181,11 +181,11 @@ func populateS3AccessDetails(initCmd *kopia.Command, repository *executor.Reposi
 	initCmd.AddArg(repository.S3Config.AccessKeyID)
 	initCmd.AddArg("--secret-access-key")
 	initCmd.AddArg(repository.S3Config.SecretAccessKey)
-	initCmd.AddArg("--sseType")
 	// At present the backuplocation CR was set with "AES256" value for SSE-S3.
 	// So need to do this conversion.
 	switch repository.S3Config.SseType {
 	case "AES256":
+		initCmd.AddArg("--sseType")
 		initCmd.AddArg("SSE-S3")
 	}
 
