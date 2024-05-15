a0112b27cb1520db9e977ea7343316dda6d596ec
Lalatendu Das <ldas@purestorage.com>
2024-05-15 06:39:51 +0000

    pb-6681: Add support for PSA in kdmp job
    - if psa enabled then extracted the uid and gid used by the POD.
      here the pod is choosen based on whichever PVC is used by that pod
    - applied those uid and GID to all relevant job pod spec
    - for baseline and privilege mode no restriction on UID/GID and
      default setting of SElinux and secomp is adopted in the job POD spec
    
    Signed-off-by: Lalatendu Das <ldas@purestorage.com>

--- pkg/drivers/nfscsirestore/nfscsirestore.go
+++ pkg/drivers/nfscsirestore/nfscsirestore.go
@@ -266,6 +266,13 @@ func jobForRestoreCSISnapshot(
 			},
 		},
 	}
+	// Add security Context only if the PSA is enabled.
+	if jobOption.PodUserId != "" || jobOption.PodGroupId != "" {
+		job, err = utils.AddSecurityContextToJob(job, jobOption.PodUserId, jobOption.PodGroupId)
+		if err != nil {
+			return nil, err
+		}
+	}
 	// Add the image secret in job spec only if it is present in the stork deployment.
 	if len(imageRegistrySecret) != 0 {
 		job.Spec.Template.Spec.ImagePullSecrets = utils.ToImagePullSecret(utils.GetImageSecretName(jobName))
